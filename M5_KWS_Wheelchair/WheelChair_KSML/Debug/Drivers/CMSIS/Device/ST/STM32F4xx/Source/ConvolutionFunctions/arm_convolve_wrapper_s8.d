ccc3e8f004d631d49b8237479bca89f191ee4a1c
Eddy960 <58588150+Eddy960@users.noreply.github.com>
2021-06-13 23:41:14 +0800

    Add files via upload

---
+++ M5_KWS_Wheelchair/WheelChair_KSML/Debug/Drivers/CMSIS/Device/ST/STM32F4xx/Source/ConvolutionFunctions/arm_convolve_wrapper_s8.d
@@ -0,0 +1,17 @@
+Drivers/CMSIS/Device/ST/STM32F4xx/Source/ConvolutionFunctions/arm_convolve_wrapper_s8.o: \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Source/ConvolutionFunctions/arm_convolve_wrapper_s8.c \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_nnfunctions.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_math_types.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_nn_types.h
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_nnfunctions.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_math_types.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_nn_types.h:
---
+++ M5_KWS_Wheelchair/WheelChair_KSML/Debug/Drivers/CMSIS/Device/ST/STM32F4xx/Source/ConvolutionFunctions/arm_depthwise_separable_conv_HWC_q7_nonsquare.d
@@ -0,0 +1,38 @@
+Drivers/CMSIS/Device/ST/STM32F4xx/Source/ConvolutionFunctions/arm_depthwise_separable_conv_HWC_q7_nonsquare.o: \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Source/ConvolutionFunctions/arm_depthwise_separable_conv_HWC_q7_nonsquare.c \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_nnfunctions.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_math_types.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_nn_types.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_nnsupportfunctions.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_common_tables.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/dsp/fast_math_functions.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_math_types.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_math_memory.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/dsp/none.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/dsp/utils.h
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_nnfunctions.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_math_types.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_nn_types.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_nnsupportfunctions.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_common_tables.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/dsp/fast_math_functions.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_math_types.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_math_memory.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/dsp/none.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/dsp/utils.h:
