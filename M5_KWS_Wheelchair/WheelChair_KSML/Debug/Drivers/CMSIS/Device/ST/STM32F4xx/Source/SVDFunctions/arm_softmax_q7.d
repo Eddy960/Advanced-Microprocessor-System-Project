8ccfc12d6c6f7cb7dc86c6c5a7710d2f01165696
Eddy960 <58588150+Eddy960@users.noreply.github.com>
2021-06-13 23:45:12 +0800

    Add files via upload

---
+++ M5_KWS_Wheelchair/WheelChair_KSML/Debug/Drivers/CMSIS/Device/ST/STM32F4xx/Source/SVDFunctions/arm_softmax_q7.d
@@ -0,0 +1,17 @@
+Drivers/CMSIS/Device/ST/STM32F4xx/Source/SVDFunctions/arm_softmax_q7.o: \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Source/SVDFunctions/arm_softmax_q7.c \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_nnfunctions.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_math_types.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_nn_types.h
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_nnfunctions.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_math_types.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.0/WheelChair_KSML/Drivers/CMSIS/Include/arm_nn_types.h:
